197db4736a16bdcf65638672a94775bcea2d1f23
kevinicb99 <kevinicb99@gmail.com>
2026-02-05 11:39:20 -0500

    Tarea 7 oauth2.0

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,26 @@
     <router-link to="/buscarId">BuscarId</router-link> |
     <router-link to="/actualizar">Actualizar</router-link> |
     <router-link to="/actualizarParcial">ActualizarParcial</router-link> |
-    <router-link to="/eliminar">Eliminar</router-link>
-   
+    <router-link to="/eliminar">Eliminar</router-link> |
+    
+    <button @click="salir" class="btn-salir">Cerrar Sesión</button>
   </nav>
   <router-view />
 </template>
+
+<script>
+import { cerrarSesionFacade } from "./clients/AuthClient.js"; 
+
+export default {
+  methods: {
+    salir() {
+      
+        cerrarSesionFacade();
+      
+    }
+  }
+}
+</script>
 
 <style scoped>
 nav {
@@ -21,9 +36,23 @@ nav a {
   font-weight: bold;
   color: #2c3e50;
   text-decoration: none;
+  margin: 0 5px;
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
+.btn-salir {
+  margin-left: 15px;
+  background-color: #ff4757;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+
+
 </style>
--- src/clients/AuthClient.js
+++ src/clients/AuthClient.js
@@ -2,18 +2,46 @@ import axios from "axios";
 
 const URL_AUTH = "http://localhost:8082/auth/token";
 
+const obtenerToken = async () => {
+    // recuperar un token
+    const tokenGuardado = localStorage.getItem("token_auth");
+
+    // si ya existe, lo retornamos
+    if (tokenGuardado) {
+        console.log("Reutilizando token del localStorage");
+        return tokenGuardado;
+    }
+
+    // si no existe pedir token
+    try {
+        console.log("Solicitando nuevo token al servidor");
+        const response = await axios.get(URL_AUTH, {
+            params: {
+                username: 'admin',
+                password: 'admin123'
+            }
+        });
+
+        const nuevoToken = response.data.accessToken;
+        
+        // guardamos para tener persistencia
+        localStorage.setItem("token_auth", nuevoToken);
+        
+        return nuevoToken;
+    } catch (error) {
+        console.error("Error en la petición de autenticación:", error);
+        throw error;
+    }
+};
+
 export const obtenerTokenFacade = async () => {
-  try {
-    const response = await axios.get(URL_AUTH, {
-      params: {
-        username: 'admin',
-        password: 'admin123'
-      }
-    });
+    return await obtenerToken();
+};
+
+export const cerrarSesionFacade = () => {
+    // Borramos el token específico
+    localStorage.removeItem("token_auth");
+    console.log("Sesión cerrada: Token eliminado");
     
-    return response.data.accessToken;
-  } catch (error) {
-    console.error("Error obteniendo el token :", error);
-    throw error;
-  }
+    
 };
